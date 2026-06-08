577ccdbe981de073faedff6713aad50f724afb56
FlaggedATX <renatosegundo840@gmail.com>
2026-06-08 00:38:26 -0300

    feature: Made the sieve functional(not true yet tho)

--- Eratosthenes.cpp
+++ Eratosthenes.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <chrono>
+#include <math.h>
+
+int TAMANHO = 1000000; //Defines the number up until we want to calculate the primes
+
+
 
 void nArray(std::vector<int>& n){
-	n.reserve(1000000);
-	for(int i = 2; i <= 1000002; i++){
-		n.push_back(i);
+	n.reserve(TAMANHO);
+	int num = 1;
+	for(int i = 0; i <= TAMANHO; i++){
+		n.push_back(num);
 	}
 }
-
-int buscaB(std::vector<int> n, int m){
+//Binary Search, ended up unused
+/*int buscaB(std::vector<int> n, int m){
 	int max = n.size();
 	int min = 0;
 	int i = (max + min) / 2;
@@ -20,28 +27,42 @@ int buscaB(std::vector<int> n, int m){
 	}
 	return i;
 }
+*/
 
-//Calcular todos os multiplos de um numero x e marcalos, pegar o menor numero depois de x nao marcado e repetir, os numeros q sobram sao primos.
+//Applies the sieve of Eratosthenes
 int main(){
+	auto start = std::chrono::high_resolution_clock::now();
+
 	std::vector<int> n;
 	nArray(n);
+	n[0] = 0;
+	n[1] = 0;
 	std::size_t max = n.size();
-	int i = 0;
+	int i = 2;
+	int m = -1;
 	do{
-		int buul = 1;
-		int m = n[i];
+		m = i;
 		int x = m;
-		while(buul == 1){
-			if(m >= max){
-				buul = 0;
-			}
-			else{
+		if(i >= 2){
+			while(m < max){
 				m = m + x;
-				int index = buscaB(n, m);
-				n.erase(n.begin() + index);
+				if(m < max){n[m] = 0;}
 			}
 		}
 		m = x;
+		//std::cout << "Rodando..." << std::endl;
 		i++;
-	}while(m <= max);
+	}while(m < sqrt(max));
+
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double, std::milli> elapsed = end - start;
+	std::cout << "Execution time: " << elapsed.count() << "ms" << std::endl;
+
+	/* Optional snippet to see the numbers
+	for(int i = 2; i < TAMANHO; i++){
+		if(n[i] == 1){
+			std::cout << i << std::endl;
+		}
+	}
+	*/ 
 }
